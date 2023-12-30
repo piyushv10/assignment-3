f6569b0584cd5d7430f40e6e0ac12427dd0e2a52
Piyush Vaidya <vaidyapiyush8@gmail.com>
2023-12-30 10:57:52 +0530

    Website Deployed

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const card = (props) => {
+    
   return (
-    <div>
-        
-       
+    <div>    
         <div>
-            
             <p>
                 {props.num}
             </p>
--- src/components/metrics.jsx
+++ src/components/metrics.jsx
@@ -14,15 +14,15 @@ const CompanyMetrics = () => {
   return (
     <div className="company-metrics-container">
       
-      <Card  num= {'406411.29'}  desc= {'Total Revenue'} />
-      <Card  num= {'620'}  desc= {'Total Jobs Avg'} />
+      <Card  num= {'$406411.29'}  desc= {'Total Revenue'} />
+      <Card  num= {'$620'}  desc= {'Total Jobs Avg'} />
       <Card  num= {'655'}  desc= {'Tickets Created'} />
       <Card  num= {'735'}  desc= {'Tickets Scheculed'} />
-      <Card  num= {'110448.8'}  desc= {'Outstanding amount'} />
+      <Card  num= {'$110448.8'}  desc= {'Outstanding amount'} />
       <Card  num= {'105'}  desc= {'Memberships Sold'} />
       <Card  num= {'436'}  desc= {'Jobs Completed'} />
       <Card  num= {'65'}  desc= {'Total Canceled'} />
-      
+
 
     </div>
   );
